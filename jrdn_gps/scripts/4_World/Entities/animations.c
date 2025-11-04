315cefa2539fbf749f5c400b20526e6873937275
jordan-0711 <jordanwoods0711@gmail.com>
2025-11-04 11:59:17 +0000

    refresh

--- jrdn_gps/scripts/4_World/Entities/animations.c
+++
@@ -1,9 +0,0 @@
-modded class ModItemRegisterCallbacks
-{
-	override void RegisterOneHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
-    {
-        super.RegisterOneHanded(pType, pBehavior);
-		pType.AddItemInHandsProfileIK("jrdn_gps_wire","dz/anims/workspaces/player/player_main/player_main_1h.asi",pBehavior,"dz/anims/anm/player/ik/gear/9v_battery.anm");
-		pType.AddItemInHandsProfileIK("jrdn_gps_pcb","dz/anims/workspaces/player/player_main/player_main_1h.asi",pBehavior,"dz/anims/anm/player/ik/gear/9v_battery.anm");
-	}
-} 
